ae7891b50b9b248d2ec9683e38f5e12443fa5666
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-21 14:50:42 +0900

    Install several middlewares

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import router from "./api";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 //middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cookieParser());
 app.use("/api", router);
 
 export default app;

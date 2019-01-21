4e5d72eeee452166665df39050893dc5aaf35394
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-21 14:47:25 +0900

    check api

--- src/api/test.js
+++ src/api/test.js
@@ -1,8 +1,16 @@
 import express from "express";
+import multer from "multer";
+
 const router = express.Router();
+const upload = multer();
 
 router.get("/", (req, res) => {
   res.send("dddd");
+});
+
+router.post("/", upload.array(), (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
 });
 
 export default router;
--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
+import bodyParser from "body-parser";
 import router from "./api";
 
 const app = express();
 
+//middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", router);
 
 export default app;

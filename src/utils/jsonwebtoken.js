df3815e6747d27710b9204fd4a42965e6177026f
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-02-03 15:36:11 +0900

    jsonwebtoken

---
+++ src/utils/jsonwebtoken.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import User from "../model/user/UserModel";
+dotenv.config();
+
+export const createJWT = async id => {
+  const token = jwt.sign({ id }, process.env.JSON_WEB_TOKEN_SECRET_KEY || "");
+  return token;
+};
+
+export const decodeJWT = async token => {
+  const decoded = jwt.verify(
+    token,
+    process.env.JSON_WEB_TOKEN_SECRET_KEY || ""
+  );
+  const { id } = decoded;
+  try {
+    const user = await User.findById(id).then(project => {
+      return project;
+    });
+    return user;
+  } catch (err) {
+    return null;
+  }
+};
